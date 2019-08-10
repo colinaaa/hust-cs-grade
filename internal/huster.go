7fdc167ab796078006b22ae7674dc401fe4a35cb
Colin <qingyu.wang@aliyun.com>
2019-08-10 12:53:54 +0800

    :truck: mv Hustermap into a single file

--- internal/huster.go
+++ internal/huster.go
@@ -58,17 +58,3 @@ func (h Huster) Overall() Term {
 func (h Huster) All() []Term {
 	return []Term{h.Fall18(), h.Spring19(), h.Overall()}
 }
-
-// HusterMap is the map with id as key and Huster struct as value
-type HusterMap map[string]Huster
-
-// FindNameByID is the method to find names
-// returns "" if not found
-func (hm HusterMap) FindNameByID(id string) string {
-	h, ok := hm[id]
-	if !ok {
-		return ""
-	}
-
-	return h.Name
-}
